e684131082c5132a8668b3efe4991bad529e8c2c
LeroyHJ <leroy_jallow_@hotmail.co.uk>
2022-08-16 17:09:31 +0100

    updated first challenge

--- spec/report_length_spec.rb
+++ spec/report_length_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe "report_length method" do
     result = report_length('hat')
     expect(result).to eq "This string was 3 characters long."
   end
+
+  it "returns 'This string was 0 characters long.' when user presses Enter" do
+    result = report_length('')
+    expect(result).to eq "This string was 0 characters long."
+  end
+
+  it "returns 'This string was 14 characters long.' when input is 'random string!'" do
+    result = report_length('random string!')
+    expect(result).to eq "This string was 14 characters long."
+  end
 end
