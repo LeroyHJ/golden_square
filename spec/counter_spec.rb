8e7e93f05537ff09bb74a7e3d7c3cc7f1059ad89
LeroyHJ <leroy_jallow_@hotmail.co.uk>
2022-08-17 10:54:02 +0100

    counter excercise

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -1,35 +1,24 @@
 require 'counter'
 
 RSpec.describe Counter do
-  it "returns the sum of the previous numbers" do
+  it "returns the sum of the first number" do
     counter = Counter.new()
     counter.add(4)
     result = counter.report()
     expect(result).to eq "Counted to 4 so far."
   end
 
-  # it do
-  #   result = 
-  #   expect(result).to eq
-  # end
+  it "returns the sum of the previous numbers" do
+    counter = Counter.new()
+    counter.add(0)
+    result = counter.report()
+    expect(result).to eq "Counted to 0 so far."
+  end
 
-  # it do
-  #   result = 
-  #   expect(result).to eq
-  # end
+  it "returns the sum of minus numbers" do
+    counter = Counter.new()
+    counter.add(-8)
+    result = counter.report()
+    expect(result).to eq "Counted to -8 so far."
+  end
 end
-
-
-# class Counter
-#   def initialize
-#     @count = 0
-#   end
-
-#   def add(num)
-#     @count += num
-#   end
-
-#   def report
-#     return "Counted to #{@count} so far."
-#   end
-# end
